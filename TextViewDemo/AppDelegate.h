425912ce2bd6e53cdca5f9f01fb80545a8fa2d7c
WZH <WZH@wzh.local>
2016-06-06 14:37:21 +0800

    Initial Commit

---
+++ TextViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextViewDemo
+//
+//  Created by WZH on 16/6/6.
+//  Copyright © 2016年 WZH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextViewDemo
+//
+//  Created by WZH on 16/6/6.
+//  Copyright © 2016年 WZH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
